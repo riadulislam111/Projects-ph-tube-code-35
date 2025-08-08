5fcf1000e81c8e18f43d41f666a896485b43e2f0
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-08-08 11:42:00 +0600

    added all file update

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,19 @@ function loadCategories() {
 }
 
 function displayCategories(categories) {
-           console.log(categories)
+           
+ const categoryContainer = document.getElementById("category-container")
+
+     for (let cat of categories) {
+        console.log(cat);
+
+        const categoryDiv = document.createElement("div");
+        categoryDiv.innerHTML = `
+        <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        `;
+        categoryContainer.appendChild(categoryDiv);
+     }
+
 }
 
 loadCategories();
